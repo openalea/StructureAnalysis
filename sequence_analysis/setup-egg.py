65dc26437cde7eeae69aba0c49efba0276ad703c
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2007-09-05 14:21:14 +0000

    Create setup-egg.py for stats modules
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@3571 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ sequence_analysis/setup-egg.py
@@ -0,0 +1,56 @@
+from setuptools import setup, find_packages
+import os, sys
+from os.path import join as pj
+ 
+packagename = 'sequence_analysis'
+namespace = 'openalea'
+
+build_prefix = "build-scons"
+
+# Scons build directory
+scons_parameters=["build_prefix="+build_prefix]
+
+
+# platform dependencies
+if("win" in sys.platform):
+    install_requires = ["boostpython"]
+    setup_requires = install_requires + []
+else:
+    install_requires = []
+    setup_requires = []
+
+
+
+if __name__ == '__main__':
+    
+    setup(name='VPlants.Sequence_analysis',
+          version='0.1',
+          author='',
+          description='sequence analysis library',
+          url='',
+          license='GPL',
+          
+          # Define where to execute scons
+          scons_scripts=['SConstruct'],
+          # Scons parameters  
+          scons_parameters=scons_parameters,
+        
+          # Packages
+          #packages=
+          #package_dir=
+      
+          # Add package platform libraries if any
+          include_package_data=True,
+          zip_safe = False,
+
+          # Specific options of openalea.deploy
+          lib_dirs = {'lib' : pj(build_prefix, 'lib'),},
+          inc_dirs = { 'include' : pj(build_prefix, 'include') },
+          
+
+          # Dependencies
+          setup_requires = setup_requires + ['openalea.deploy'],
+          install_requires = install_requires,
+          dependency_links = ['http://openalea.gforge.inria.fr/pi'],
+          )
+
---
+++ tool/setup-egg.py
@@ -0,0 +1,55 @@
+from setuptools import setup, find_packages
+import os, sys
+from os.path import join as pj
+ 
+packagename = 'tool'
+namespace = 'openalea'
+build_prefix = "build-scons"
+
+# Scons build directory
+scons_parameters=["build_prefix="+build_prefix]
+
+
+# dependencies
+if("win" in sys.platform):
+    install_requires = []
+    setup_requires = install_requires + []
+else:
+    install_requires = []
+    setup_requires = []
+
+
+
+if __name__ == '__main__':
+    
+    setup(name='VPlants.Tool',
+          version='0.1',
+          author='',
+          author_email='',
+          description='vplants tools',
+          url='',
+          license='GPL',
+ 
+          # Define where to execute scons
+          scons_scripts=['SConstruct'],
+          # Scons parameters  
+          scons_parameters=scons_parameters,
+        
+          # Packages
+          #packages=
+          #package_dir=
+      
+          # Add package platform libraries if any
+          include_package_data=True,
+          zip_safe = False,
+
+          # Specific options of openalea.deploy
+          lib_dirs = {'lib' : pj(build_prefix, 'lib'),},
+          inc_dirs = { 'include' : pj(build_prefix, 'include') },
+          
+
+          # Dependencies
+          setup_requires = setup_requires + ['openalea.deploy'],
+          install_requires = install_requires,
+          dependency_links = ['http://openalea.gforge.inria.fr/pi'],
+          )
