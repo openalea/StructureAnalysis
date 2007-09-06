201944a3010832073d559ea2f4ddaaa22bc059b9
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2007-09-06 17:09:46 +0000

    Stat_tool : create base file to implement python wrappers
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@3578 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ stat_tool/src/wrapper/export_distribution.cpp
@@ -0,0 +1,34 @@
+/*------------------------------------------------------------------------------
+ *                                                                              
+ *        VPlants.Stat_Tool : VPlants Statistics module
+ *                                                                              
+ *        Copyright 2006-2007 INRIA - CIRAD - INRA                      
+ *                                                                              
+ *        File author(s): Yann Guédon <yann.guedon@cirad.fr>
+ *                        Jean-Baptiste Durand <Jean-Baptiste.Durand@imag.fr>
+ *                        Samuel Dufour-Kowalski <samuel.dufour@sophia.inria.fr>
+ *                        Christophe Pradal <christophe.prada@cirad.fr>         
+ *                                                                              
+ *        Distributed under the GPL 2.0 License.                               
+ *        See accompanying file LICENSE.txt or copy at                          
+ *           http://www.gnu.org/licenses/gpl-2.0.txt
+ *                                                                              
+ *        OpenAlea WebSite : http://openalea.gforge.inria.fr                    
+ *       
+ *        $Id: scenecontainer_wrap.cpp 559 2007-05-25 12:25:30Z dufourko $
+ *                                                                       
+ *-----------------------------------------------------------------------------*/
+
+#include "export_distribution.h"
+
+
+#include <boost/python.hpp>
+using namespace boost::python;
+
+
+// Boost.Python Wrapper export function
+void class_distribution()
+{
+
+}
+
