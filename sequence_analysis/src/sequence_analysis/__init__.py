a3c265b5234cb4ce1a4cb8d125d222ccbfe1e478
Jean-Baptiste Durand <Jean-Baptiste.Durand@imag.fr>
2008-03-26 17:59:49 +0000

    Move wrappers for stat_tool and sequence_analysis to directories associated with those modules
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@4592 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ sequence_analysis/src/sequence_analysis/__init__.py
@@ -0,0 +1,2 @@
+from sequence import *
+del os, stat_tool, csequence
---
+++ sequence_analysis/src/sequence_analysis/sequence.py
@@ -0,0 +1,7 @@
+"""Classes shared by most statistical modules
+"""
+
+
+import openalea.stat_tool as stat_tool, os
+import csequence
+VariableType=csequence.VariableType
