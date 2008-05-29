9f31f6a3de649609c3788e195ffc2bf175a64049
Samuel Dufour-Kowalski <samuel.dufour@cirad.fr>
2008-05-29 10:29:52 +0000

    fix options.py
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@4962 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- tool/options.py
+++ tool/options.py
@@ -5,13 +5,6 @@
 import sys
 if('win' in sys.platform):
     compiler='mingw'
-    if compiler == 'mingw':
-        gl_includes = r'C:\MinGW\include\GL'
-        gl_lib = r'C:\MinGW\lib' 
-        QTDIR=r'C:\Qt\4.2.3'
-        QT_VERSION='4'
-    else: 
-        EXTRA_LIBS = 'advapi32.lib user32.lib gdi32 shell32.lib'
 
 else:
     QTDIR="/usr"
