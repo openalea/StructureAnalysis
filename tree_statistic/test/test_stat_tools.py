43276c5178e651e6446cb82554acb702d67b4859
Thomas Cokelaer <cokelaer@gmail.com>
2008-12-11 09:21:48 +0000

    fixes the import that was failing.
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@5844 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- tree_statistic/test/test_stat_tools.py
+++ tree_statistic/test/test_stat_tools.py
@@ -1,5 +1,4 @@
-import openalea.stat_tool
-stat_tool=openalea.stat_tool
+import openalea.stat_tool as stat_tools
 
 inf_bound=0
 sup_bound=3
@@ -9,18 +8,18 @@ sample= []
 val= [0]
 ident=stat_tools.DistributionIdentifier.UNIFORM
 parameter=stat_tools.D_DEFAULT
-distrib= stat_tools.Parametric(ident, inf_bound, sup_bound, 
+distrib= stat_tools._ParametricModel(ident, inf_bound, sup_bound, 
                                parameter, probability)
 print "A uniform distribution:"
 print distrib
 print "Simulation from this distribution: "
-print distrib.simulation()
+print distrib.simulate()
 for i in range(sample_size):
-    val[0]= distrib.simulation()
+    val[0]= distrib.simulate()
     sample= sample+val
 print sample
 print "Histogram build from this distribution: "
 distrib_data=stat_tools.Histogram(sample)
 print distrib_data
-distrib_data.Display(Detail=2)
+distrib_data.display(Detail=2)
 # distrib_data.Plot()
