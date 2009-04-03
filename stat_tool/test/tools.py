5676cd28d6b16ffd1c933a676c2ffa0391abe98b
Thomas Cokelaer <cokelaer@gmail.com>
2009-04-03 16:25:14 +0000

    add stat tool test interface to ease ABC with all data structure
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@6179 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ stat_tool/test/tools.py
@@ -0,0 +1,157 @@
+from openalea.stat_tool import *
+from openalea.stat_tool.plot import DISABLE_PLOT
+
+class interface():
+    """Interface to be used by test file that perform tests on the following
+    data structure: compound, convolution, mixture, histogram, vector
+    
+    :param data: a data that will be filled using the build_data structure
+    :para filename: a filename to a file containing the relevant data structure
+    :param structure: reference to a data structure Class that is not instantiated.
+    
+    :Usage:
+    In you test file, add ::
+        
+        >>> from tools import interface
+        
+    Then, if we consider the Compound class case, create a class as follows::
+     
+        class Test(interface):
+            def __init__(self):
+                self.data = self.build_data()
+                self.filename = "compound1.cd"
+                self.structure = Compound  
+        
+            def build_data(self):
+                d1 = Binomial(2, 5, 0.5)
+                d2 = NegativeBinomial(0, 2, 0.5)
+                data = Compound(d1, d2) 
+                return data
+        
+            def test_empty(self):
+                self.empty()
+        
+    """
+    def __init__(self, data, filename, structure_object):
+        self.data = None
+        self.filename = None
+        self.structure_object = None
+        
+    def build_data(self):
+        raise NotImplementedError()
+    
+    def empty(self):
+        """Test that empty constructor fails"""
+        try:
+            _m = self.structure()
+            assert False
+        except TypeError:
+            assert True
+    
+    def constructor_from_file(self):
+        """Test constructor from file"""
+        c = self.structure(self.filename)
+        assert c
+        return c
+    
+    def constructor_from_file_failure(self):
+        """run constructor with filename argument"""
+        try:
+            _h = self.structure("whatever_wrong_filename.txt")
+            assert False
+        except Exception:
+            assert True
+
+    def print_data(self):
+        """test that print command exists"""
+        print self.data
+        
+    def display(self):
+        """check that .display/Display is callable"""
+        data = self.data
+        data.display()
+        Display(data)
+        assert data.display()==Display(data)
+        
+    def display_versus_ascii_write(self):
+        """check that display is equivalent to ascii_write"""
+        Display(self.data) == self.data.ascii_write(False) 
+
+    def display_versus_str(self):
+        """check that display and str are equivalent"""
+        data = self.data
+        s = str(data)
+        assert Display(data) == s
+        
+    def plot(self):
+        """run plotting routines """
+        if DISABLE_PLOT == False:
+            self.data.plot()
+            
+    def save(self, Format=None):
+        """In the Vector case, Format should be Data. 
+        
+        .. todo:: This is surely a bug. to be checked"""
+        c1 = self.data  
+
+        
+        if Format is None:
+            c1.save('test1.dat')
+            Save(c1, 'test2.dat')
+        else:
+            c1.save('test1.dat', Format="Data")
+            Save(c1, 'test2.dat', Format="Data")
+        
+        c1_read = self.structure('test1.dat')
+        c2_read = self.structure('test2.dat')
+        
+        assert c1 and c1_read and c2_read
+        assert str(c1_read) == str(c2_read)
+        
+        os.remove('test1.dat')
+        os.remove('test2.dat')
+    
+    def plot_write(self):
+        h = self.data
+        h.plot_write('test', 'title')
+
+    def file_ascii_write(self):
+        h = self.data
+        h.file_ascii_write('test.dat', True)
+        os.remove('test.dat')
+        
+    def file_ascii_data_write(self):
+        h = self.data
+        h.file_ascii_data_write('test.dat', True)
+        os.remove('test.dat')
+        
+    def spreadsheet_write(self):
+        h = self.data
+        h.spreadsheet_write('test.dat')
+        os.remove('test.dat')
+        
+    def survival_ascii_write(self):
+        d = self.data
+        d.survival_ascii_write()
+        
+    def survival_spreadsheet_write(self):
+        d = self.data
+        d.survival_spreadsheet_write('test.xsl')
+        os.remove('test.xsl')
+        
+    def simulate(self):
+        """Test the simulate method"""
+        m = self.data
+        s = m.simulate(1000)
+        assert len(s) == 1000
+        assert str(s)
+        return s
+    
+    def test_len(self):
+        raise NotImplementedError() 
+
+    def test_extract(self):
+        raise NotImplementedError() 
+    
+    def test_extract_data(self):
+        raise NotImplementedError()
