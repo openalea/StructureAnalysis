89c6a1fcac190ba3ce8d505b333418b88c9f3b60
Thomas Cokelaer <cokelaer@gmail.com>
2009-04-20 16:42:02 +0000

    update the setup and the scons files
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@6253 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- sequence_analysis/setup.py
+++ sequence_analysis/setup.py
@@ -1,8 +1,32 @@
+# -*- coding: utf-8 -*-
+
 import os, sys
 from os.path import join as pj
 
 from setuptools import setup, find_packages
 from openalea.deploy.binary_deps import binary_deps
+
+
+
+
+
+name = 'VPlants.Sequence_analysis'
+namespace = 'openalea'
+# to get the version
+version="0.6.2"
+description = 'sequence analysis library',
+long_description = """"""
+authors = 'Y. Guédon',
+author_email = 'samuel.dufour@sophia.inria.fr, christophe.pradal@cirad.fr'
+url = 'http://www-sop-inria.fr/virtualplants'
+license = 'GPL'
+__revision__ = "$Id: setup.py 6086 2009-03-13 16:24:30Z cokelaer $"
+
+
+
+
+
+
 
 packagename = 'sequence_analysis'
 namespace = 'openalea'
@@ -25,12 +49,12 @@ if sys.platform.startswith('win'):
 
 if __name__ == '__main__':
 
-    setup(name='VPlants.Sequence_analysis',
-          version='0.6.2',
-          author='Yann Guedon',
-          description='sequence analysis library',
-          url='http://www-sop.inria.fr/virtualplants/',
-          license='GPL',
+    setup(name=name,
+          version=version,
+          author=authors,
+          description=description,
+          url=url,
+          license=license,
 
           # Define where to execute scons
           scons_scripts=['SConstruct'],
@@ -42,9 +66,14 @@ if __name__ == '__main__':
 
           # Packages
           packages=['openalea.sequence_analysis'],
-          package_dir={'openalea.sequence_analysis' : 'src/sequence_analysis',
+          
+
+           #package_dir={'openalea.sequence_analysis' : 'src/openalea/sequence_analysis',
                        #'' : 'build/lib', # hack to use develop command
-                       },
+#                       },
+
+          package_dir={ "" : "src"  },
+
 
           # Add package platform libraries if any
           include_package_data=True,
