aac87225f4027f91847f6b465e5b730c1a1e9413
Thomas Cokelaer <cokelaer@gmail.com>
2009-04-23 12:40:02 +0000

    move  python files into a new src/tree_reduction directory and update the setup.py file
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@6267 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- tree_reduction/setup.py
+++ tree_reduction/setup.py
@@ -1,60 +1,47 @@
-"""setup file for stat_tool package"""
+"""setup file for tree reduction package"""
 import os, sys
 from setuptools import setup, find_packages
 from openalea.deploy.binary_deps import binary_deps
 from os.path import join as pj
+__revision__ = "$Id: setup.py 6086 2009-03-13 16:24:30Z cokelaer $"
 
 
 
 name = 'VPlants.tree_reduction'
-namespace = 'vplants'
-# to get the version
-version="0.6.2"
-description = 'tree reduction' 
+namespace = 'openalea'
+pkg_name = 'openalea.tree_reduction'
+version = '0.6.2'
+description = 'tree reduction.'
 long_description = """"""
-author = 'Farah Ben-naoum',
+author = 'Farah Ben-naoum'
 author_email = ''
 url = 'http://openalea.gforge.inria.fr'
-license = 'GPL' 
-__revision__ = "$Id: setup.py 6086 2009-03-13 16:24:30Z cokelaer $"
+license = 'Cecill-C'
+
+packages = [ namespace+"."+pkg for pkg in find_packages('src') if 'openalea' not in pkg]
 
 
-packagename = 'tree_reduction'
+setup(
+    name=name,
+    version=version,
+    description=description,
+    author=author,
+    author_email=author_email,
+    url=url,
+    license=license,
 
-# platform dependencies
+    namespace_packages=['openalea'],
+    create_namespaces = True,
+    zip_safe = False,
 
-setup_requires = ['openalea.core']
+    packages = packages,
+    package_dir={ pkg_name : pj('src','tree_reduction'), '' : 'src' },
 
+    # Dependencies
+    install_requires = ['openalea.deploy'],
+    dependency_links = ['http://openalea.gforge.inria.fr/pi'],
 
-if __name__ == '__main__':
-    
-    setup(name=name,
-          version=version,
-          description=description,
-          long_description=long_description,
-          author=author,
-          author_email=author_email,
-          url=url,
-          license=license,
- 
-#          namespace_packages=['vplants'],
-          create_namespaces=False,
-
-          # Packages
-          packages=find_packages('src'),
-
-          package_dir={ "" : "src"  },
-          
-          # Add package platform libraries if any
-          include_package_data=True,
-          
-          zip_safe = False,
-
-          # Dependencies
-          setup_requires = setup_requires,
-          install_requires = [],
-          dependency_links = ['http://openalea.gforge.inria.fr/pi'],
-          )
+    )
 
 
     
