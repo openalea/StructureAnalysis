021a2945f829cb99b12be06c738084b85ce97f6a
Farah Ben-Naoum <ben_naoumfr@yahoo.fr>
2009-04-23 10:08:02 +0000

    update setup.py
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@6264 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- tree_reduction/setup.py
+++ tree_reduction/setup.py
@@ -7,12 +7,12 @@ from os.path import join as pj
 
 
 name = 'VPlants.tree_reduction'
-namespace = 'openalea'
+namespace = 'vplants'
 # to get the version
 version="0.6.2"
 description = 'tree reduction' 
 long_description = """"""
-author = 'Rarah Ben-naoum',
+author = 'Farah Ben-naoum',
 author_email = ''
 url = 'http://openalea.gforge.inria.fr'
 license = 'GPL' 
@@ -20,18 +20,10 @@ __revision__ = "$Id: setup.py 6086 2009-03-13 16:24:30Z cokelaer $"
 
 
 packagename = 'tree_reduction'
-build_prefix = "build-scons"
-
-# Scons build directory
-scons_parameters = ["build_prefix=" + build_prefix]
-
 
 # platform dependencies
-install_requires = [binary_deps('vplants.tool')]
-if sys.platform.startswith('win'):
-    install_requires += [binary_deps("boostpython")]
 
-setup_requires = install_requires + ['openalea.deploy']
+setup_requires = ['openalea.core']
 
 
 if __name__ == '__main__':
@@ -45,37 +37,22 @@ if __name__ == '__main__':
           url=url,
           license=license,
  
-          
-          # Define where to execute scons
-          scons_scripts=['SConstruct'],
-          # Scons parameters  v
-          scons_parameters=scons_parameters,
-
-          namespace_packages=['openalea'],
+          namespace_packages=['vplants'],
           create_namespaces=True,
 
           # Packages
-          packages=['openalea.stat_tool', 
-                    'openalea.stat_tool.demo', 
-                    'vplants',
-                    ],
+          packages=['vplants.tree_reduction',],
 
           package_dir={ "" : "src"  },
           
           # Add package platform libraries if any
           include_package_data=True,
-          package_data = {'' : ['*.pyd', '*.so', '*.dylib'],},
           
           zip_safe = False,
 
-          # Specific options of openalea.deploy
-          lib_dirs = {'lib' : pj(build_prefix, 'lib'),},
-          inc_dirs = { 'include' : pj(build_prefix, 'include') },
-          
-
           # Dependencies
           setup_requires = setup_requires,
-          install_requires = install_requires,
+          install_requires = [],
           dependency_links = ['http://openalea.gforge.inria.fr/pi'],
           )
 
