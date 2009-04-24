9f5497639b5fc0de7502f41a808a6f66c1d15d43
Thomas Cokelaer <cokelaer@gmail.com>
2009-04-24 10:53:20 +0000

    add new module top_parameters and update tops
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@6286 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ sequence_analysis/src/openalea/sequence_analysis/top_parameters.py
@@ -0,0 +1,42 @@
+"""Tops"""
+__revision__ = "$Id: vectors.py 6217 2009-04-08 12:40:15Z cokelaer $"
+
+import os
+import openalea.stat_tool.interface as interface
+import _sequence_analysis
+
+from _sequence_analysis import _Top_parameters
+
+__all__ = ['Top_parameters',
+           '_Top_parameters']
+
+
+# Extend dynamically class
+interface.extend_class( _Top_parameters, interface.StatInterface)
+
+# Add methods to _Vectors
+
+
+def Top_parameters(*args, **kargs):
+    """todo
+
+    """
+    
+    if len(args)==1: 
+        #filename case
+        if isinstance(args[0], str):
+            filename = args[0]
+            if os.path.isfile(filename):
+                return _Top_parameters(filename, True)
+            else:
+                raise IOError("bad file name")
+        #sequences case
+    else:
+        raise TypeError("Expected a valid filename or a list of lists (e.g., [[1,0],[0,1]])")
+    
+    
+
+ 
+    
+
+
