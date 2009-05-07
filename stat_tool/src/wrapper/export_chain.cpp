d94c49326f1df95860cb5f6590811b0337d35b1e
Thomas Cokelaer <cokelaer@gmail.com>
2009-05-07 16:14:03 +0000

    update wrapping in stat_tool
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@6386 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- stat_tool/src/wrapper/export_chain.cpp
+++ stat_tool/src/wrapper/export_chain.cpp
@@ -42,11 +42,17 @@ void class_chain()
 	class_< Chain >("_Chain", "Chain")
     .def(init<char, int , bool>())
     .def(init<char, int, int , bool>())
-/*    .def_readonly("get_type", &Chain::get_type, "get type")
-    .def_readonly("get_nb_state", &Chain::get_nb_state, "get nb state")
-    .def_readonly("get_nb_row", &Chain::get_nb_row, "get nb_row")
-    .def_readonly("get_nb_component", &Chain::get_nb_row, "get nb_component")
-    .def("get_component_nb_state", &Chain::component_nb_state, "component nb state")
+    .def(init< optional<char, int, bool> >())
+
+    .def(self_ns::str(self)) //__str__
+
+    .add_property("type", &Chain::type, "returns type")
+    .add_property("nb_state", &Chain::nb_state, "returns nb_state")
+    .add_property("nb_row", &Chain::nb_row, "returns nb_row")
+    .add_property("nb_component", &Chain::nb_row, "returns nb_component")
+
+/*
+ *     .def("get_component_nb_state", &Chain::component_nb_state, "component nb state")
     .def("get_state_type", &Chain::get_state_type, "get state type")
     .def("get_initial", &Chain::get_initial, "get initial")
 */
@@ -54,32 +60,40 @@ void class_chain()
     ;
 
 /*
+    void init(bool left_right , double self_transition);
 
-bool get_accessibility(int state1 , int state2) const{ return accessibility[state1][state2]; }
-int get_component(int icomponent , int index) const{ return component[icomponent][index]; }
-double get_transition(int memory , int state) const{ return transition[memory][state]; } 
-bool **accessibility;   // matrice d'accessibilite des etats
-int *component_nb_state;  // nombre d'etats par classe
-int **component;        // classes
-char *state_type;       // types des etats ('r' : recurrent,
-double *cumul_initial;  // fonction de repartition correspondant
-double **transition;    // matrice des probabilites de transition
-double **cumul_transition;  // fonctions de repartition correspondant aux lignes
-void create_cumul();
-void cumul_computation();
-void remove_cumul();
-void log_computation();
-bool** logic_transition_computation() const;
-bool connex_component_research(Format_error &error , bool **ilogic_transition = 0) const;
-void graph_accessibility_computation(bool **ilogic_transition);
-void probability_accessibility_computation();
-void component_computation(bool **ilogic_transition = 0);
-void thresholding(double min_probability);
-int nb_parameter_computation(double min_probability = 0.) const;
-double chi2_value_computation(const Chain_data &chain_data) const;
-void init(bool left_right , double self_transition);
-double likelihood_computation(const Chain_data &chain_data , bool initial_flag = true) const;
-void chi2_fit(const Chain_data &chain_data , Test &test) const;
+    double likelihood_computation(const Chain_data &chain_data , bool initial_flag = true) const;
+    void chi2_fit(const Chain_data &chain_data , Test &test) const;
+
+    bool **accessibility;   // matrice d'accessibilite des etats
+    int *component_nb_state;  // nombre d'etats par classe
+    int **component;        // classes
+    char *state_type;       // types des etats ('r' : recurrent,
+                            // 't' : transitoire, 'a' : absorbant)
+    double *initial;        // probabilites initiales
+    double *cumul_initial;  // fonction de repartition correspondant
+                            // au probabilites initiales
+    double **transition;    // matrice des probabilites de transition
+    double **cumul_transition;  // fonctions de repartition correspondant aux lignes
+                                // de la matrice des probabilites de transition
+
+     std::ostream& ascii_print(std::ostream &os , bool file_flag = false) const;
+    std::ostream& spreadsheet_print(std::ostream &os) const;
+    void create_cumul();
+    void cumul_computation();
+    void remove_cumul();
+    void log_computation();
+
+    bool** logic_transition_computation() const;
+    bool connex_component_research(Format_error &error , bool **ilogic_transition = 0) const;
+    void graph_accessibility_computation(bool **ilogic_transition);
+    void probability_accessibility_computation();
+    void component_computation(bool **ilogic_transition = 0);
+
+    void thresholding(double min_probability);
+
+    int nb_parameter_computation(double min_probability = 0.) const;
+    double chi2_value_computation(const Chain_data &chain_data) const;
 
 */
 
