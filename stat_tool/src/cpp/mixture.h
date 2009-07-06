0b479c143059e1e084709f4758a67f68fe53d4ed
Yann Guédon <guedon@cirad.fr>
2009-07-06 06:28:03 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@6605 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- stat_tool/src/cpp/mixture.h
+++ stat_tool/src/cpp/mixture.h
@@ -98,7 +98,7 @@ private :
     std::ostream& spreadsheet_write(std::ostream &os , const Mixture_data *mixt_histo) const;
     bool plot_write(const char *prefix , const char *title ,
                     const Mixture_data *mixt_histo) const;
-    plotable::MultiPlotSet* get_plotable(const Mixture_data *mixt_histo) const;
+    MultiPlotSet* get_plotable(const Mixture_data *mixt_histo) const;
 
     int nb_parameter_computation() const;
     double penalty_computation() const;
@@ -132,7 +132,7 @@ public :
     bool spreadsheet_write(Format_error &error , const char *path) const;
     bool plot_write(Format_error &error , const char *prefix ,
                     const char *title = 0) const;
-    plotable::MultiPlotSet* get_plotable() const;
+    MultiPlotSet* get_plotable() const;
 
 /*    RWDECLARE_COLLECTABLE(Mixture);
 
@@ -204,7 +204,7 @@ public :
     bool spreadsheet_write(Format_error &error , const char *path) const;
     bool plot_write(Format_error &error , const char *prefix ,
                     const char *title = 0) const;
-    plotable::MultiPlotSet* get_plotable() const;
+    MultiPlotSet* get_plotable() const;
 
 /*    RWDECLARE_COLLECTABLE(Mixture_data);
 
