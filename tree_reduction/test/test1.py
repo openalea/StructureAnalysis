431d68518171f41073ace4635494d78d3b1fcdc4
Farah Ben-Naoum <ben_naoumfr@yahoo.fr>
2009-09-05 14:58:40 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@6866 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- tree_reduction/test/test1.py
+++ tree_reduction/test/test1.py
@@ -1,25 +1,17 @@
 # -*- coding: cp1252 -*-
-
-"""from amlPy import *
+"""
+from amlPy import *
 
 from vplants.self_similarity.graph import *
 from vplants.self_similarity.cst import *
-from vplants.self_similarity.mtg2graph import *"""
-
+from vplants.self_similarity.mtg2graph import *
+"""
 from vplants.tree_reduction.graph import *
+from vplants.tree_reduction.functions import *
 from openalea.container.graph import *
 import string
 
-"""mtg_filename = './example_MTG.mtg'
 
-g = MTG(mtg_filename)
-
-print 'Create the graph'
-tree = mtg2graph(1, 2)
-
-print 'Reduction'
-dag_exact = tree_reduction(tree)
-tree_rec = tree_reconstruction(tree)"""
 
 """   rec directe
 def test1():
@@ -31,7 +23,7 @@ def test1():
     assert g.nb_edges() == 7
     return g"""
 
-""" rec indirecte"""
+""" rec indirecte
 def test1():
     edges=[(1,2),(1,7),(2,3),(2,6),(3,4),(3,7),(4,6),(4,5),(5,7),(6,8),(7,8)]
     edges_weight={0:1,1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:1,9:3,10:2}
@@ -39,7 +31,7 @@ def test1():
     g = from_edges(root, edges, edges_weight)
     assert g.nb_vertices() == 8
     assert g.nb_edges() == 11
-    return g
+    return g"""
 
 """ perfectly included paths
 def test1():
@@ -59,6 +51,108 @@ def test1():
     g = from_edges(root, edges, edges_weight)
     assert g.nb_vertices() == 9
     assert g.nb_edges() == 14
+    return g"""
+
+
+"""   rice panicle
+def test1():
+    edges=[(1,2),(2,3),(2,4),(3,5),(4,6),(4,7),(5,8),(5,9),(6,10),(7,11),(7,12),(8,13),(9,20),(9,25),(10,106),(11,53),(12,64),(12,74),(13,14),(14,15),(15,16),(16,17),(17,18),(18,19),(19,100),(20,21),(20,28),(21,22),(22,23),(23,24),(24,94),(24,101),(25,26),(26,27),(27,28),(28,104),(29,30),(30,31),(31,32),(32,33),(33,34),(34,35),(35,93),(35,89),(36,37),(36,45),(37,38),(38,39),(39,40),(40,41),(41,42),(42,43),(42,101),(43,44),(43,93),(44,93),(45,46),(46,47),(47,48),(48,49),(49,50),(50,51),(51,52),(51,90),(52,89),(52,90),(53,28),(53,54),(54,55),(54,104),(55,56),(56,57),(57,58),(58,59),(59,60),(59,101),(60,61),(60,93),(61,62),(61,93),(62,63),(62,88),(63,87),(63,101),(64,65),(65,66),(65,28),(66,67),(67,68),(68,69),(69,70),(70,71),(70,93),(71,72),(71,99),(72,73),(72,99),(73,87),(73,88),(74,75),(75,76),(76,77),(77,78),(78,79),(79,80),(80,81),(81,82),(81,100),(82,83),(82,98),(83,84),(83,90),(84,85),(84,90),(85,86),(85,88),(86,101),(86,100),(87,101),(87,103),(88,93),(88,102),(89,90),(89,91),(90,88),(90,102),(91,92),(91,102),(93,101),(93,104),(94,95),(94,93),(95,96),(95,99),(96,97),(96,98),(97,98),(97,100),(98,100),(98,102),(99,102),(99,87),(100,93),(100,103),(101,102),(101,104),(102,103),(102,104),(103,104),(104,105)]
+    edges_weight={0:1,1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:1,9:1,10:1,11:1,12:1,13:1,14:1,15:1,16:1,17:1,18:1,19:1,20:1,21:1,22:1,23:1,24:1,25:1,26:1,27:1,28:1,29:1,30:1,31:1,32:1,33:1,34:1,35:1,36:1,37:1,38:1,39:1,40:1,41:1,42:1,43:1,44:1,45:1,46:1,47:1,48:1,49:1,50:1,51:1,52:1,53:1,54:1,55:1,56:1,57:1,58:1,59:1,60:1,61:1,62:1,63:1,64:1,65:1,66:1,67:1,68:1,69:1,70:1,71:1,72:1,73:1,74:1,75:1,76:1,77:1,78:1,79:1,80:1,81:1,82:1,83:1,84:1,85:1,86:1,87:1,88:1,89:1,90:1,91:1,92:1,93:1,94:1,95:1,96:1,97:1,98:1,99:1,100:1,101:1,102:1,103:1,104:1,105:1,106:1,107:1,108:1,109:1,110:1,111:1,112:1,113:1,114:1,115:1,116:1,117:1,118:1,119:1,120:1,121:1,122:1,123:1,124:1,125:1,126:1,127:1,128:1,129:1,130:1,131:1,132:1,133:1,134:1,135:1,136:1,137:1,138:1,139:1,140:1,141:1,142:1,143:1,144:1,145:1,146:1,147:1,148:1,149:1}
+    root = 1
+    g = from_edges(root, edges, edges_weight)
+    assert g.nb_vertices() == 106
+    assert g.nb_edges() == 150
+    return g"""
+
+
+"""   NEST of rice panicle
+def test1():
+    edges=[(1,2),(2,3),(2,7),(3,4),(3,5),(4,5),(4,8),(5,6),(5,9),(6,7),(7,8),(7,13),(8,9),(8,11),(9,10),(9,20),(10,11),(10,23),(11,12),(12,13),(13,14),(13,19),(14,15),(14,18),(15,16),(15,18),(16,17),(16,18),(17,18),(18,19),(19,20),(20,21),(20,23),(21,22),(21,24),(22,23),(22,24),(23,24),(24,25)]
+    edges_weight={0:1,1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:1,9:1,10:1,11:1,12:1,13:1,14:1,15:1,16:1,17:1,18:1,19:1,20:1,21:1,22:1,23:1,24:1,25:1,26:1,27:1,28:1,29:1,30:1,31:1,32:1,33:1,34:1,35:1,36:1,37:1,38:1}
+    root = 1
+    g = from_edges(root, edges, edges_weight)
+    assert g.nb_vertices() == 25
+    assert g.nb_edges() == 39
+    return g"""
+
+"""    T0
+def test1():
+    edges=[(1,2) ,(1,4) ,(2,3) ,(2,4) ,(3,4) ,(4,5),(4,7) ,(5,6) ,(5,7) ,(6,7) ,(7,8) ,(7,10) ,(8,9) ,(8,10) ,(9,10) ,(10,11)]
+    edges_weight={0:1,1:1,2:1,3:1,4:2,5:1,6:1,7:1,8:1,9:2,10:1,11:1,12:1,13:1,14:2,15:1}
+    root = 1
+    g = from_edges(root, edges, edges_weight)
+    assert g.nb_vertices() == 11
+    assert g.nb_edges() == 16
+    return g"""
+
+"""   T0,4
+def test1():
+    edges=[(1,2),(2,3),(2,4),(3,5),(3,6),(4,7),(4,15),(5,8),(5,17),(6,9),(6,17),(7,10),(7,15),(8,11),(8,13),(9,12),(9,15),(10,15),(11,14),(11,17),(12,17),(13,16),(14,18),(14,22),(15,19),(15,22),(16,21),(17,19),(18,20),(18,22),(19,21),(19,22),(20,22),(21,22),(22,23)]
+    edges_weight={0:1,1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:1,9:1,10:1,11:1,12:1,13:1,14:1,15:1,16:1,17:2,18:1,19:1,20:2,21:1,22:1,23:1,24:1,25:1,26:1,27:1,28:1,29:1,30:1,31:1,32:2,33:2,34:1}
+    root = 1
+    g = from_edges(root, edges, edges_weight)
+    assert g.nb_vertices() == 23
+    assert g.nb_edges() == 35
+    return g"""
+
+"""   T0,8
+def test1():
+    edges=[(1,2),(2,3),(3,4),(4,5),(4,6),(5,7),(5,9),(6,8),(7,9),(8,10),(8,13),(9,11),(9,13),(10,13),(11,12),(12,13),(13,14)]
+    edges_weight={0:1,1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:2,9:1,10:1,11:1,12:1,13:2,14:1,15:2,16:1}
+    root = 1
+    g = from_edges(root, edges, edges_weight)
+    assert g.nb_vertices() == 14
+    assert g.nb_edges() == 17
+    return g"""
+
+"""    T1
+def test1():
+    edges=[(1,2),(1,4),(2,3),(2,4),(3,4),(4,5),(4,7),(5,6),(5,7),(6,7),(7,8),(7,10),(8,9),(8,10),(9,10),(10,11),(11,12),(12,13),(12,14),(13,14),(14,15),(14,16),(15,16),(16,17)]
+    edges_weight={0:1,1:1,2:1,3:1,4:2,5:1,6:1,7:1,8:1,9:2,10:1,11:1,12:1,13:1,14:2,15:1,16:2,17:1,18:1,19:2,20:1,21:1,22:2,23:1}
+    root = 1
+    g = from_edges(root, edges, edges_weight)
+    assert g.nb_vertices() == 17
+    assert g.nb_edges() == 24
+    return g"""
+
+"""    T2
+def test1():
+    edges=[(1,2),(1,10),(2,3),(2,10),(3,4),(3,10),(4,5),(4,7),(5,6),(5,7),(6,7),(7,8),(7,10),(8,9),(8,10),(9,10),(10,11),(10,12),(11,12),(12,13),(12,14),(13,14),(14,15),(14,16),(15,16),(16,17)]
+    edges_weight={0:1,1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:1,9:1,10:2,11:1,12:1,13:1,14:1,15:2,16:1,17:1,18:2,19:1,20:1,21:2,22:1,23:1,24:2,25:1}
+    root = 1
+    g = from_edges(root, edges, edges_weight)
+    assert g.nb_vertices() == 17
+    assert g.nb_edges() == 26
+    return g"""
+
+"""    T3
+def test1():
+    edges=[(1,2),(2,3),(3,4),(4,5),(5,6),(6,7)]
+    edges_weight={0:3,1:3,2:3,3:3,4:3,5:4}
+    root = 1
+    g = from_edges(root, edges, edges_weight)
+    assert g.nb_vertices() == 7
+    assert g.nb_edges() == 6
+    return g"""
+
+"""    palmier
+def test1():
+    edges=[(1,2),(2,3),(3,4),(4,5),(5,6),(6,7),(6,10),(7,8),(7,11),(8,9),(8,12),(9,13),(10,11),(11,12),(12,13)]
+    edges_weight={0:1,1:1,2:1,3:1,4:1,5:1,6:2,7:1,8:2,9:1,10:2,11:3,12:1,13:1,14:1}
+    root = 1
+    g = from_edges(root, edges, edges_weight)
+    assert g.nb_vertices() == 13
+    assert g.nb_edges() == 15
+    return g"""
+
+"""    Borchet and Honda model
+def test1():
+    edges=[(1,2),(2,3),(2,14),(3,4),(3,14),(4,5),(4,14),(5,6),(5,14),(6,7),(6,14),(7,8),(7,14),(8,9),(8,14),(9,10),(9,14),(10,11),(10,14),(11,12),(11,14),(12,13),(12,15),(13,16),(14,15),(14,17),(15,16),(15,17),(16,17)]
+    edges_weight={0:1,1:1,2:1,3:1,4:1,5:1,6:1,7:1,8:1,9:1,10:1,11:1,12:1,13:1,14:1,15:1,16:1,17:1,18:1,19:1,20:1,21:1,22:1,23:2,24:1,25:1,26:1,27:1,28:2}
+    root = 1
+    g = from_edges(root, edges, edges_weight)
+    assert g.nb_vertices() == 17
+    assert g.nb_edges() == 29
     return g"""
 
 def signat_vert_comp(g):
@@ -137,13 +231,15 @@ def MQPP(vid, g, signat_edges):
     '''
     '''
     M=[]
+    stop=False
     QIP=[]
     for eid in Quasi_isom_desc_vertices(vid, g, signat_edges):
         QIP.append([eid])
-    while (len(QIP)>1):
+    while (len(QIP)>1) and stop==False:
         if len(QIP)==0:
             QPP=QIP[:]
         else:
+            """construire QPP et tester si QPP est valable et donc arret"""
             QPP=[]
             QPP.append(QIP[0][:])
             Q=QIP[:]
@@ -152,43 +248,78 @@ def MQPP(vid, g, signat_edges):
                 elt=QPP[len(QPP)-1]
                 eid=elt[len(elt)-1]
                 i=0
-                while i<len(Q):
+                trouv=False
+                while trouv==False and i<len(Q):
                     eid2=Q[i][0]
                     if g._edges[eid][1]==g._edges[eid2][0]:
                         QPP.append(Q[i])
                         Q=Q[0:i]+Q[i+1:]
+                        trouv=True
                         a=True
                     else:
                         i=i+1
-                        a=False
+                if trouv==False:
+                    a=False                        
             if len(QPP)>1:
-                M.append(QPP)
-
-        vid2=g._edges[QIP[0][len(QIP[0])-1]][1]
-        i=0
-        while i<len(QIP):
-            eid=QIP[i][len(QIP[i])-1]
-            vid=g._edges[eid][1]
-            add=False
-            for eid2 in Quasi_isom_desc_vertices(vid2, g, signat_edges):
-                vid3=g._edges[eid2][0]
-                if vid==vid3:
-                    QIP[i].append(eid2)
-                    add=True
-                    break
-            if add==False:
-                QIP=QIP[0:i]+QIP[i+1:]
-            else:
-                i=i+1
-            
+                M=M+QPP
+                stop=True
+        """ ralonger les paths de QIP"""
+        if stop==False:
+            vid2=g._edges[QIP[0][len(QIP[0])-1]][1]
+            i=0
+            while i<len(QIP):
+                eid=QIP[i][len(QIP[i])-1]
+                vid=g._edges[eid][1]
+                add=False
+                for eid2 in Quasi_isom_desc_vertices(vid2, g, signat_edges):
+                    vid3=g._edges[eid2][0]
+                    if vid==vid3:
+                        QIP[i].append(eid2)
+                        add=True
+                        break
+                if add==False:
+                    QIP=QIP[0:i]+QIP[i+1:]
+                else:
+                    i=i+1        
     return M
+
+def paths_inclusion(p1, p2, g):
+    '''
+    '''
+    P=[]
+    for i in range(len(p1)):
+        P=P+p1[i]       
+    Q=[]
+    for i in range(len(p2)):
+        Q=Q+p2[i]
+    inclusion=False
+    if len(P)<len(Q):
+        cmp=0
+        for i in range(len(P)):
+            if P[i] in Q:
+                cmp=cmp+1
+        if cmp==len(P):
+                inclusion=True
+    return inclusion
 
 def MQPPRT(g, signat_edges):
     '''
     '''
     MRT=[]
     for vid in g.vertices():
-        MRT=MRT+MQPP(vid, g, signat_edges)
+        M=MQPP(vid, g, signat_edges)
+        ok=True
+        for i in range(len(MRT)):
+            if paths_inclusion(M, MRT[i], g):
+                ok=False
+        if ok:
+            MRT.append(M)
+            i=0
+            while i<len(MRT)-1:
+                if paths_inclusion(MRT[i], M, g):
+                    MRT.remove(MRT[i])
+                else:
+                    i=i+1         
     return MRT
         
 def paths_intersection(p1, p2, g):
@@ -226,6 +357,7 @@ def MQPPRT_classes(g, paths):
     for i in range(len(indep)):
         if indep[i]==True:
             MQPPRTINDEP.append(paths[i])
+
     return MQPPRTINDEP, MQPPRTOVERL
                 
 def path_wining(g, P):
@@ -251,98 +383,10 @@ def overlap_select(g, MQPPRTOVERL):
                 select.append(MQPPRTOVERL[i][1])
         elif MQPPRTOVERL[i][0] not in select:
             select.append(MQPPRTOVERL[i][0])
+
     return select
 
 """GRT creation and GRTLre derivation"""
-
-def equal_string(a,b):
-    if (a=='alpha' and b=='alpha'):
-        e=False
-    elif a=='' and b=='':
-        e=True
-    elif (a=='' and b!='') or (a!='' and b==''):
-        e=False
-    else:
-        sub=a[0:4]
-        nsub=string.count(a,sub)
-        if nsub!=string.count(b,sub):
-            e=False
-        else:
-            a=string.replace(a,sub,'')
-            b=string.replace(b,sub,'')
-            e=equal_string(a,b)
-    return e
-
-def sup_edge(x,y,list_edge,list_node,G_edge,nbr_edge):
-    """supprime l'arc entre les noeuds x et y"""
-    k=ind_find(x,list_edge)
-    if y in list_edge[k][1]:
-        list_edge[k][1].remove(y)
-        if (nbr_edge[G_edge[(list_node[x],list_node[y])]]>=1):
-            nbr_edge[G_edge[(list_node[x],list_node[y])]]-=1
-        else:
-            nbr_edge.pop(G_edge[(list_node[x],list_node[y])])
-        G_edge.pop((list_node[x],list_node[y]))
-    return list_edge,G_edge,nbr_edge
-
-def nbr_parent(i,list_edge):
-    """retourne le nombre des noeuds parents du noeud i"""
-    k=0
-    nbr=0
-    while k<len(list_edge):
-        if i in list_edge[k][1]:
-            nbr=nbr+1
-        k=k+1
-    return nbr
-
-def list_parent(i,list_edge):
-    """retourne le nombre des noeuds parents du noeud i"""
-    k=0
-    lp=[]
-    while k<len(list_edge):
-        if i in list_edge[k][1]:
-            lp.append(list_edge[k][0])
-        k=k+1
-    return lp
-        
-def ind_find(x,list_edge):
-    """recherche l'indice dans list_edge de l'element associé au noeud x
-    (pour accéder a la liste de ces fils)"""
-    k=0
-    while (list_edge[k][0]!=x) and (k<len(list_edge)):
-            k=k+1  
-    return k
-
-def ajust_sup(list_edge,G_edge,list_node,G_node,nbr_edge):
-    """Après la suppression d'un arc le graphe G a besoin d'un ajustement
-    en supprimant tous les nœuds inaccessibles qui ne possède aucun parent"""
-    k=1
-    mod=True
-    while mod==True:
-        mod=False
-        while (k<len(list_edge)):
-            if (nbr_parent(list_edge[k][0],list_edge)==0):
-                mod=True
-                G_node.pop(list_node[list_edge[k][0]])
-                k1=0
-                while (k1<len(list_edge[k][1])):
-                    if (nbr_edge[G_edge[(list_node[list_edge[k][0]],list_node[list_edge[k][1][k1]])]]>=1):
-                        nbr_edge[G_edge[(list_node[list_edge[k][0]],list_node[list_edge[k][1][k1]])]]-=1
-                    else:
-                        nbr_edge.pop(G_edge[(list_node[list_edge[k][0]],list_node[list_edge[k][1][k1]])])
-                    G_edge.pop((list_node[list_edge[k][0]],list_node[list_edge[k][1][k1]]))
-                    k1+=1
-                list_node=list_node[0:list_edge[k][0]]+list_node[list_edge[k][0]+1:len(list_node)]
-                for k1 in range(len(list_edge)):
-                    if list_edge[k1][0]>list_edge[k][0]:
-                        list_edge[k1][0]-=1
-                    for k2 in range(len(list_edge[k1][1])):
-                        if list_edge[k1][1][k2]>list_edge[k][0]:
-                            list_edge[k1][1][k2]-=1
-                list_edge=list_edge[0:k]+list_edge[k+1:len(list_edge)]
-            else: k+=1
-    return list_edge,G_edge,list_node,G_node,nbr_edge
-
 
 def DAG_init(g):
     node =[]
@@ -360,13 +404,6 @@ def DAG_init(g):
     for eid in g.edges():
         val[g._edges[eid]]=g._edge_property['weight'][eid]    
     return node,edge,val
-  
-def creer_symbol(V): #ajouter un nouveau symbole correspondant a un noeud
-  c=''
-  for c in string.lowercase+string.uppercase:
-    if c not in V and c!='[' and c!='F' and c!=']' and c!='l' and c!='W':
-      break
-  return c
         
 def G_construction(node_list,edge_list,val_edges):
     """ a partir du DAG construire le graphe G équivalent, G est représenté par:
@@ -419,23 +456,6 @@ def G_construction(node_list,edge_list,val_edges):
           G_edge[G_edge.keys()[k]]=nbr_edge.keys()[j]    
     return list_node,list_edge,G_node,G_edge,nbr_edge,parameters,parameters1,parameters2
     
-def nbr_arc_construction(G_edge):
-    nbr_edge={}
-    d=G_edge.values()
-    while d!=[]:
-        val=d[0]
-        d.remove(val)
-        nbr=1
-        k=0
-        while (k<len(d)):
-          if equal_string(val,d[k]):
-            nbr=nbr+1
-            d.remove(d[k])
-          else: k=k+1  
-        nbr_edge[val]=nbr
-    nbr_edge['alpha']=1
-    return nbr_edge
-
 
 def quotient_graph(g,list_node,list_edge,G_node,G_edge,nbr_edge,parameters,parameters1,parameters2, setP):
     '''
@@ -448,7 +468,6 @@ def quotient_graph(g,list_node,list_edge,G_node,G_edge,nbr_edge,parameters,param
             m=[]
             for k in range(len(P[j])):
                 eid=P[j][k]
-                print '********'
                 vid1=g._edges[eid][0]
                 vid2=g._edges[eid][1]
                 if list_node[vid1-1] not in m:
@@ -594,6 +613,17 @@ def Parametric_L_system(G_node,list_node,parameters,parameters1,parameters2):
                 print list_node[i],'(',symbol,')',':',symbol,'=0 ->',G_node[list_node[i]][1]
 
 if(__name__ == "__main__"):
+    """
+    mtg_filename = './example_MTG.mtg'
+
+    g = MTG(mtg_filename)
+
+    print 'Create the graph'
+    tree = mtg2graph(1, 2)
+
+    print 'Reduction'
+    dag_exact = tree_reduction(tree)
+    tree_rec = tree_reconstruction(tree)"""
     G=test1()
     S=signat_vert_comp(G)
     E=signat_edg_comp(G, S)
