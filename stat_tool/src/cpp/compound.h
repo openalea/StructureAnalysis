dabd68621860db6adecf5f014909c26d28b14dd8
Yann Guédon <guedon@cirad.fr>
2010-02-18 07:29:01 +0000

    new class naming convention
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@8030 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- stat_tool/src/cpp/compound.h
+++ stat_tool/src/cpp/compound.h
@@ -66,15 +66,15 @@ const int COMPOUND_COEFF = 10;         // coefficient arrondi estimateur
  */
 
 
-class Histogram;
-class Compound_data;
+class FrequencyDistribution;
+class CompoundData;
 
 
-// class Compound : public STAT_interface , protected Distribution {
-class Compound : public STAT_interface , public Distribution {  // loi composee
+// class Compound : public StatInterface , protected Distribution {
+class Compound : public StatInterface , public Distribution {  // loi composee
 
-    friend class Histogram;
-    friend class Compound_data;
+    friend class FrequencyDistribution;
+    friend class CompoundData;
 
     friend Compound* compound_ascii_read(Format_error &error , const char *path ,
                                          double cumul_threshold);
@@ -83,36 +83,36 @@ class Compound : public STAT_interface , public Distribution {  // loi composee
 
 private :
 
-    Compound_data *compound_data;  // pointeur sur un objet Compound_data
-    Parametric *sum_distribution;  // loi de la somme
-    Parametric *distribution;  // loi elementaire
+    CompoundData *compound_data;  // pointeur sur un objet CompoundData
+    DiscreteParametric *sum_distribution;  // loi de la somme
+    DiscreteParametric *distribution;  // loi elementaire
 
     void copy(const Compound &compound , bool data_flag = true);
 
-    std::ostream& ascii_write(std::ostream &os , const Compound_data *compound_histo ,
+    std::ostream& ascii_write(std::ostream &os , const CompoundData *compound_histo ,
                               bool exhaustive , bool file_flag) const;
-    std::ostream& spreadsheet_write(std::ostream &os , const Compound_data *compound_histo) const;
+    std::ostream& spreadsheet_write(std::ostream &os , const CompoundData *compound_histo) const;
     bool plot_write(const char *prefix , const char *title ,
-                    const Compound_data *compound_histo) const;
-    MultiPlotSet* get_plotable(const Compound_data *compound_histo) const;
+                    const CompoundData *compound_histo) const;
+    MultiPlotSet* get_plotable(const CompoundData *compound_histo) const;
 
-    void computation(Parametric **power_dist , int min_nb_value ,
+    void computation(DiscreteParametric **power_dist , int min_nb_value ,
                      double cumul_threshold , bool sum_flag , bool dist_flag);
-    void expectation_step(const Histogram &histo , Parametric **power_dist ,
+    void expectation_step(const FrequencyDistribution &histo , DiscreteParametric **power_dist ,
                           Reestimation<double> *sum_reestim , Reestimation<double> *reestim) const;
 
 public :
 
     Compound();
-    Compound(const Parametric &sum_dist , const Parametric &dist ,
+    Compound(const DiscreteParametric &sum_dist , const DiscreteParametric &dist ,
              double cumul_threshold = COMPOUND_THRESHOLD);
-    Compound(const Parametric &sum_dist , const Parametric &dist , char type);
+    Compound(const DiscreteParametric &sum_dist , const DiscreteParametric &dist , char type);
     Compound(const Compound &compound , bool data_flag = true)
     :Distribution(compound) { copy(compound , data_flag); }
     ~Compound();
     Compound& operator=(const Compound &compound);
 
-    Compound_data* extract_data(Format_error &error) const;
+    CompoundData* extract_data(Format_error &error) const;
 
     std::ostream& line_write(std::ostream &os) const;
 
@@ -127,13 +127,13 @@ public :
     void computation(int min_nb_value = 1 ,
                      double cumul_threshold = COMPOUND_THRESHOLD ,
                      bool sum_flag = true , bool dist_flag = true);
-    Compound_data* simulation(Format_error &error , int nb_element) const;
+    CompoundData* simulation(Format_error &error , int nb_element) const;
 
     // acces membres de la classe
 
-    Compound_data* get_compound_data() const { return compound_data; }
-    Parametric* get_sum_distribution() const { return sum_distribution; }
-    Parametric* get_distribution() const { return distribution; }
+    CompoundData* get_compound_data() const { return compound_data; }
+    DiscreteParametric* get_sum_distribution() const { return sum_distribution; }
+    DiscreteParametric* get_distribution() const { return distribution; }
 };
 
 
@@ -142,34 +142,34 @@ Compound* compound_ascii_read(Format_error &error , const char *path ,
 
 
 
-// class Compound_data : public STAT_interface , protected Histogram {
-class Compound_data : public STAT_interface , public Histogram {  // structure de donnees correspondant
-                                                                  // a une loi composee
-    friend class Histogram;
+// class CompoundData : public StatInterface , protected FrequencyDistribution {
+class CompoundData : public StatInterface , public FrequencyDistribution {  // structure de donnees correspondant
+                                                                            // a une loi composee
+    friend class FrequencyDistribution;
     friend class Compound;
 
-    friend std::ostream& operator<<(std::ostream &os , const Compound_data &compound_histo)
+    friend std::ostream& operator<<(std::ostream &os , const CompoundData &compound_histo)
     { return compound_histo.ascii_write(os , false); }
 
 private :
 
     Compound *compound;     // pointeur sur un objet Compound
-    Histogram *sum_histogram;   // histogramme correspondant a la loi de la somme
-    Histogram *histogram;   // histogramme correspondant a la loi elementaire
+    FrequencyDistribution *sum_frequency_distribution;   // loi empirique de la somme
+    FrequencyDistribution *frequency_distribution;   // loi empirique elementaire
 
-    void copy(const Compound_data &compound_histo , bool model_flag = true);
+    void copy(const CompoundData &compound_histo , bool model_flag = true);
 
 public :
 
-    Compound_data();
-    Compound_data(const Histogram &histo , const Compound &icompound);
-    Compound_data(const Compound &icompound);
-    Compound_data(const Compound_data &compound_histo , bool model_flag = true)
-    :Histogram(compound_histo) { copy(compound_histo , model_flag); }
-    virtual ~Compound_data();
-    Compound_data& operator=(const Compound_data &compound_histo);
+    CompoundData();
+    CompoundData(const FrequencyDistribution &histo , const Compound &icompound);
+    CompoundData(const Compound &icompound);
+    CompoundData(const CompoundData &compound_histo , bool model_flag = true)
+    :FrequencyDistribution(compound_histo) { copy(compound_histo , model_flag); }
+    virtual ~CompoundData();
+    CompoundData& operator=(const CompoundData &compound_histo);
 
-    Distribution_data* extract(Format_error &error , char type) const;
+    DiscreteDistributionData* extract(Format_error &error , char type) const;
 
     std::ostream& line_write(std::ostream &os) const;
 
@@ -184,8 +184,8 @@ public :
     // acces membres de la classe
 
     Compound* get_compound() const { return compound; }
-    Histogram* get_sum_histogram() const { return sum_histogram; }
-    Histogram* get_histogram() const { return histogram; }
+    FrequencyDistribution* get_sum_frequency_distribution() const { return sum_frequency_distribution; }
+    FrequencyDistribution* get_frequency_distribution() const { return frequency_distribution; }
 };
 
 
