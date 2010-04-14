e95637c3c901e9055aa1b12db0ffa6c946161c5a
Thomas Cokelaer <cokelaer@gmail.com>
2010-04-14 06:50:44 +0000

    remove useless piece of code in the init
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@8634 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- stat_tool/src/openalea/stat_tool/__init__.py
+++ stat_tool/src/openalea/stat_tool/__init__.py
@@ -5,34 +5,16 @@
 #
 #       Copyright 2008 INRIA - CIRAD
 #
-#       File author(s): 
+#       File author(s):
 #           Samuel Dufour-Kowalski <samuel.dufour@sophia.inria.fr>
 #           Yann Guedon <yann.guedon@cirad.fr>
+#           Thomas Cokelaer <Thomas.Cokelaer@sophia.inria.fr>
 #
 #       Distributed under the GPL 2.0 License.
 #       See accompanying file LICENSE.txt or copy at
 #           http://www.gnu.org/licenses/gpl-2.0.txt
-# 
-"""Stat_Tool"""
-
-# Test prefix
-
-def get_test_file(filename=""):
-    """
-    look for data in different directories
-    """
- 
-    path1 = os.path.abspath("../../../test/")
-    path2 = os.path.abspath("./test/")
-    path3 = os.path.abspath("./")
-
-    if os.path.isdir(path1):
-        return os.path.join(path1, filename)
-    elif os.path.isdir(path2):
-        return os.path.join(path2, filename)
-    else:
-        return os.path.join(path3, filename)
-
+#
+"""Stat_Tool init file"""
 
 
 import _stat_tool
@@ -53,8 +35,6 @@ from output import  *
 from data_transform import *
 from cluster import *
 
-#from error import FormatError as FormatError
-
 # Constant
 from _stat_tool import I_DEFAULT
 from _stat_tool import D_DEFAULT
@@ -65,4 +45,4 @@ from _stat_tool import VariableType
 from _stat_tool import VariableTypeBis
 from _stat_tool import RestorationAlgorithm
 
-#
+
