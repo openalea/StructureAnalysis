71e37f55115481d378f6cace7a764e3de8893f7a
Thomas Cokelaer <cokelaer@gmail.com>
2010-05-03 08:41:03 +0000

    add so README for the status of the tests fix dataflow tests and mvMixture tests
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@8792 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ stat_tool/test/README.txt
@@ -0,0 +1,28 @@
+test_cluster_and_distance_matrix.py     DONE
+test_cluster.py                         DONE
+test_comparison.py                      DONE
+test_compound_functional.py             DONE
+test_compound.py                        DONE
+test_convolution_functional.py          DONE
+test_convolution.py                     DONE
+test_dataflow_stat.py                   DONE
+test_data_transform.py                  TOBECLEANED (remove markov and co)
+test_distance_matrix.py                 TOBECLEANED
+test_distribution_functional.py         DONE
+test_distribution.py                    DONE
+test_error.py                           DONE
+test_estimate.py                        TOBEFINALISED
+test_histogram.py                       DONE
+test_matrix.py                          TOBECLEANED/FINALISED
+test_mixture_functional.py              DONE
+test_mixture.py                         DONE
+test_multivariate_mixture_functional.py TOBEFINALISED
+test_multivariate_mixture.py            TOBEFINALISED
+test_output.py                          TOBEFINALISED
+test_plot.py                            TOBEFINALISED
+test_regression_functional.py           DONE
+test_regression.py                      TOBEFINALISED
+test_simulate.py
+test_vector_distance.py
+test_vectors_functional.py              DONE
+test_vectors.py                         DONE
--- stat_tool/test/test_multivariate_mixture.py
+++ stat_tool/test/test_multivariate_mixture.py
@@ -19,13 +19,13 @@ from tools import runTestClass
 
 class Test(interface):
     """a simple unittest class"""
-   
+
     def __init__(self):
         interface.__init__(self,
                            self.build_data(),
                            "data/mixture_mv1.mixt",
                            _MultivariateMixture)
-        
+
     def build_data(self):
         d11 = Binomial(0, 12, 0.1)
         d12 = Binomial(0, 12, 0.5)
@@ -34,18 +34,18 @@ class Test(interface):
         d21 = Poisson(0, 18.0)
         d22 = Poisson(0, 5.0)
         d23 = Poisson(0, .20)
-        
+
         data = _MultivariateMixture([0.1, 0.2, 0.7], [[d11, d21], [d12, d22], [d13, d23]])
         assert data.nb_component == 3
         assert data.nb_variable == 2
         return data
-       
+
     def test_empty(self):
         """skip test_empty
-        
+
         because there is an empty constructor in VectorDistance; """
         pass
-        
+
     def _test_constructor_from_file(self):
         #raise error (proba non equal to 1) when nosetests used from parent directory.
         self.constructor_from_file()
@@ -55,44 +55,44 @@ class Test(interface):
 
     def test_print(self):
         self.print_data()
-        
+
     def test_display(self):
         self.display()
         self.display_versus_ascii_write()
-        self.display_versus_str()     
-  
+        self.display_versus_str()
+
     def test_len(self):
         c = self.data
         assert len(c) == 3
 
     def test_plot(self):
-        if DISABLE_PLOT == False:    
+        if DISABLE_PLOT == False:
             self.data.plot(1)
          #   self.data.plot(2)
 
     def _test_save(self):
         self.save()
-                
+
     def test_plot_write(self):
         self.plot_write()
 
     def test_file_ascii_write(self):
         self.file_ascii_write()
-      
+
     def test_spreadsheet_write(self):
         self.spreadsheet_write()
-    
+
     def test_simulate(self):
-        self.simulate()     
-        
+        self.simulate()
+
     def test_extract(self):
         pass
-    
+
     def test_extract_data(self):
         pass
 
     def test_simulate2(self):
-        
+
         d11 = Binomial(0, 12, 0.1)
         d12 = Binomial(0, 12, 0.5)
         d13 = Binomial(0, 12, 0.8)
@@ -110,10 +110,10 @@ class Test(interface):
 
     def _test_permutation(self):
         data1 = self.data
-        
+
         data2 = data1.state_permutation([0, 2, 1])
         _data3 = data2.state_permutation([0, 1, 2])
-        
+
         assert str(data1)==str(data2)
 
 
