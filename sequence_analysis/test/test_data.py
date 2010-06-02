9a8db86ecfaef775e69ed4d75efb028b1e57fd0d
Thomas Cokelaer <cokelaer@gmail.com>
2010-06-02 11:25:04 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@9051 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- sequence_analysis/test/test_data.py
+++ sequence_analysis/test/test_data.py
@@ -4,7 +4,7 @@ def test():
     path = data.path
     seq = data.data_tutorial.seq1
 
-def test_read_data():
+def _test_read_data():
     path = data.path
     from openalea.sequence_analysis.sequences import Sequences
     seq = Sequences(path + 'sequences_tutorial.dat')
