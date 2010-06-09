ac0e10d54d81c61caf1c111406b556d75b1953c6
Jean-Baptiste Durand <Jean-Baptiste.Durand@imag.fr>
2010-06-09 07:56:09 +0000

    utilitaires pour les wrappers
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@9112 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ tree_statistic/src/wrapper/wrapper_util.h
@@ -0,0 +1,59 @@
+// WRAPPER COMMON CLASSES
+
+
+#ifndef __WRAPPER_UTIL
+#define __WRAPPER_UTIL
+
+#include <boost/python.hpp>
+#include <boost/python/detail/api_placeholder.hpp>
+
+#include "stat_tool/stat_tools.h"
+
+using namespace boost::python;
+
+namespace tree_statistic
+{
+  namespace wrap_util
+  {
+     // New error Type
+     // static object StatTreeErrorClass;
+     object StatTreeError;
+/*
+     void throw_stat_tree_error(const char* error_message)
+     {
+       PyErr_SetString(StatTreeErrorClass.ptr(), error_message);
+       boost::python::throw_error_already_set();
+     };
+
+     // Throw exception StatTreeError with a message
+     void throw_stat_tree_error(ostringstream& error_message)
+     {
+       PyErr_SetString(StatTreeErrorClass.ptr(), (error_message.str()).c_str());
+       boost::python::throw_error_already_set();
+     }; */
+
+     void throw_stat_tree_error(object StatTreeError, const char* error_message)
+     {
+       PyErr_SetString(StatTreeError.ptr(), error_message);
+       boost::python::throw_error_already_set();
+     };
+
+     // Throw exception StatTreeError with a message
+     void throw_stat_tree_error(object StatTreeError, ostringstream& error_message)
+     {
+       PyErr_SetString(StatTreeError.ptr(), (error_message.str()).c_str());
+       boost::python::throw_error_already_set();
+     };
+
+     // Throw python predefined exception with a message
+     void throw_python_error(PyObject* exception_class,
+                             ostringstream& error_message)
+     {
+       PyErr_SetString(exception_class, (error_message.str()).c_str());
+       boost::python::throw_error_already_set();
+     };
+  }
+}
+
+
+#endif
