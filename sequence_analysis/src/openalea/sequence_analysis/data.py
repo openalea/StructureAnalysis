3a2a004354ad94cd57d0cb9191d246421c1ab5eb
Thomas Cokelaer <cokelaer@gmail.com>
2010-07-26 16:39:44 +0000

    update docstring
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@9311 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- sequence_analysis/src/openalea/sequence_analysis/data.py
+++ sequence_analysis/src/openalea/sequence_analysis/data.py
@@ -1,13 +1,17 @@
+"""
+provides data samples to play with in test and documentation
+"""
+
 import os
-from openalea.sequence_analysis import Sequences
-import openalea.sequence_analysis as t
+import openalea.sequence_analysis as sa
 
 
-path = t.__path__[0] + (os.sep + '..')*3 \
+path = sa.__path__[0] + (os.sep + '..') * 3 \
     + os.sep + 'share' + os.sep + 'data' +os.sep
 
 class _data_tutorial():
     def __init__(self):
+        from openalea.sequence_analysis import Sequences
         self.seq1 = Sequences([1,2,3,4,5,6,7,8,9,10])
 
 
