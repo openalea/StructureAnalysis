acac3cad12166e015c18ebeab6915e1981613c2a
Daniel Barbeau <daniel.barbeau@sophia.inria.fr>
2011-08-22 16:59:52 +0000

    fix docstrings
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@11002 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- sequence_analysis/test/test_dataflow_stat.py
+++ sequence_analysis/test/test_dataflow_stat.py
@@ -3,14 +3,14 @@ from openalea.core.alea import *
 #from openalea.grapheditor import dataflowview
 
 pm = PackageManager()
-pm.init(verbose=False)
+pm.init(verbose=True)
 
 # These tests use gnuplot interface, which requires human interaction
 # Consequently, they cannot be used within builbot (which hangs forever)
 # We added a flags inside aml/src/aml/wralea/py_stat.py to prevent gnuplot
 # to be launched if these tests are run with nosetests. The remaining of the
 # nodes are run.
-# In order to have the gnuplot interface, run this script with python instead of nosetests 
+# In order to have the gnuplot interface, run this script with python instead of nosetests
 
 def test_demo_corsican():
     """ Test changepoint demo corsican  """
@@ -23,12 +23,12 @@ def test_demo_dycorinia():
     assert res == []
 
 def test_oak_demo():
-    """ Test dataflow demo compound tutorial"""
+    """ Test dataflow demo oak"""
     res = run(('demo.changepoint_stat_tool', 'oak_demo'),{},pm=pm)
     assert res == []
 
 def test_beech1_demo():
-    """ Test dataflow demo compound tutorial"""
+    """ Test dataflow demo beech"""
     res = run(('demo.changepoint_stat_tool', 'beech1'),{},pm=pm)
     assert res == []
 
@@ -36,7 +36,7 @@ def test_stat_tool_demos_and_tutorial_convolution():
     """ Test dataflow demo compound tutorial"""
     res = run(('demo.stat_tool demos and tutorials', 'convolution_tutorial'),{},pm=pm)
     assert res == []
-    
+
 
 def test_stat_tool_demos_and_tutorial_compound():
     res = run(('demo.stat_tool demos and tutorials', 'compound_tutorial'),{},pm=pm)
