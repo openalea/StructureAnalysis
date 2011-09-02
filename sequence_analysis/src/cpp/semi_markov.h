d8ae00366e92479fb2de7a2ef6a12c7705902a74
Yann Guédon <guedon@cirad.fr>
2011-09-02 16:29:44 +0000

    Introduction of state sequence entropies
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@11061 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- sequence_analysis/src/cpp/semi_markov.h
+++ sequence_analysis/src/cpp/semi_markov.h
@@ -280,7 +280,9 @@ private :
     ChainData *chain_data;  // etats initaux et transitions
     double likelihood;      // vraisemblance des sequences
     double hidden_likelihood;  // vraisemblance de toutes les sequences possibles
+    double sample_entropy;  // entropie des sequences d'etats
     double *posterior_probability;  // probabilite a posteriori de la sequence d'etats la plus probable
+    double *entropy;        // entropie des sequences d'etats
 
     void copy(const SemiMarkovData &seq , bool model_flag = true);
 
@@ -322,7 +324,9 @@ public :
     ChainData* get_chain_data() const { return chain_data; }
     double get_likelihood() const { return likelihood; }
     double get_hidden_likelihood() const { return hidden_likelihood; }
+    double get_sample_entropy() const { return sample_entropy; }
     double get_posterior_probability(int index) const { return posterior_probability[index]; }
+    double get_entropy(int index) const { return entropy[index]; }
 };
 
 
