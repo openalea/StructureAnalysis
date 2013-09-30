a65c7d86a621ae61a2211a9546774ee92ea042f4
Yann Guédon <guedon@cirad.fr>
2013-09-30 14:57:33 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@15038 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- stat_tool/src/wrapper/export_mixture.cpp
+++ stat_tool/src/wrapper/export_mixture.cpp
@@ -25,7 +25,7 @@
 #include "stat_tool/stat_tools.h"
 #include "stat_tool/distribution.h"
 #include "stat_tool/convolution.h"
-#include "stat_tool/mixture.h"
+#include "stat_tool/discrete_mixture.h"
 #include "stat_tool/markovian.h"
 #include "stat_tool/multivariate_mixture.h"
 #include "stat_tool/compound.h"
