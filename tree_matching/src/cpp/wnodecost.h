13e8d8b8dbe75b22c97043968b49ee4c350ae7c4
Yann Guédon <guedon@cirad.fr>
2014-03-17 15:02:28 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@16103 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- tree_matching/src/cpp/wnodecost.h
+++ tree_matching/src/cpp/wnodecost.h
@@ -43,6 +43,7 @@
 #include "nodecost.h"
 //#include "mdtable.h"
 #include "stat_tool/stat_tools.h"
+#include "stat_tool/markovian.h"
 #include "stat_tool/vectors.h"
 
 const DistanceType NOTDEF=-1.0;
