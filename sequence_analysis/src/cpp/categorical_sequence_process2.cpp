fccd3edc85077496a700363b56f0b9cbf111c832
Yann Guédon <guedon@cirad.fr>
2015-04-23 09:32:23 +0000

    Introduction of namspaces stat_tool and sequence_analysis
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@18044 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- sequence_analysis/src/cpp/categorical_sequence_process2.cpp
+++ sequence_analysis/src/cpp/categorical_sequence_process2.cpp
@@ -3,7 +3,7 @@
  *
  *       V-Plants: Exploring and Modeling Plant Architecture
  *
- *       Copyright 1995-2014 CIRAD/INRA/Inria Virtual Plants
+ *       Copyright 1995-2015 CIRAD/INRA/Inria Virtual Plants
  *
  *       File author(s): Yann Guedon (yann.guedon@cirad.fr)
  *
@@ -41,7 +41,10 @@
 
 #include "stat_tool/stat_tools.h"
 #include "stat_tool/curves.h"
+#include "stat_tool/distribution.h"
 #include "stat_tool/markovian.h"
+#include "stat_tool/vectors.h"
+#include "stat_tool/distance_matrix.h"
 #include "stat_tool/stat_label.h"
 
 #include "sequences.h"
@@ -49,11 +52,10 @@
 #include "tool/config.h"
 
 using namespace std;
+using namespace stat_tool;
 
 
-extern int column_width(int value);
-extern int column_width(int nb_value , const double *value , double scale = 1.);
-extern char* label(const char *file_name);
+namespace sequence_analysis {
 
 
 
@@ -4123,3 +4125,6 @@ void CategoricalSequenceProcess::plotable_write(MultiPlotSet &plot , int &index 
     }
   }
 }
+
+
+};  // namespace sequence_analysis
