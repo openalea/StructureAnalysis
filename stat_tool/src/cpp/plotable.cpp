2b65c26dd561c781179e65a37b6847e660512d21
Yann Guédon <guedon@cirad.fr>
2015-04-23 07:02:57 +0000

    Introduction of namespace stat_tool and removing of namespace plotable
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@18012 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- stat_tool/src/cpp/plotable.cpp
+++ stat_tool/src/cpp/plotable.cpp
@@ -1,11 +1,49 @@
+/* -*-c++-*-
+ *  ----------------------------------------------------------------------------
+ *
+ *       V-Plants: Exploring and Modeling Plant Architecture
+ *
+ *       Copyright 1995-2015 CIRAD/INRA/Inria Virtual Plants
+ *
+ *       File author(s): Samuel Dufour-Kowalski, Yann Guedon (yann.guedon@cirad.fr)
+ *
+ *       $Source$
+ *       $Id: plotable.cpp 16835 2014-06-12 06:30:51Z guedon $
+ *
+ *       Forum for V-Plants developers:
+ *
+ *  ----------------------------------------------------------------------------
+ *
+ *                      GNU General Public Licence
+ *
+ *       This program is free software; you can redistribute it and/or
+ *       modify it under the terms of the GNU General Public License as
+ *       published by the Free Software Foundation; either version 2 of
+ *       the License, or (at your option) any later version.
+ *
+ *       This program is distributed in the hope that it will be useful,
+ *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *       MERCHANTABILITY or FITNESS For A PARTICULAR PURPOSE. See the
+ *       GNU General Public License for more details.
+ *
+ *       You should have received a copy of the GNU General Public
+ *       License along with this program; see the file COPYING. If not,
+ *       write to the Free Software Foundation, Inc., 59
+ *       Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ *  ----------------------------------------------------------------------------
+ */
+
+
 #include <iostream>
 #include <stdlib.h>
 
 #include "plotable.h"
 
-using namespace plotable;
 using namespace std;
 
+
+namespace stat_tool {
 
 
 //////////////////////// SinglePlot /////////////////////////////////////////////
@@ -79,3 +117,6 @@ MultiPlotSet::MultiPlotSet(int size, int inb_variable)
 {
   nb_variable = inb_variable;
 }
+
+
+};  // namespace stat_tool
