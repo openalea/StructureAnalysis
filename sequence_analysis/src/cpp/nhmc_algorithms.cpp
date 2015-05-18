c61a195574c2d2b976becf4ef870726e33386de6
Yann Guédon <guedon@cirad.fr>
2015-05-18 15:14:02 +0000

    Change of header inclusion design
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@18266 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- sequence_analysis/src/cpp/nhmc_algorithms.cpp
+++ sequence_analysis/src/cpp/nhmc_algorithms.cpp
@@ -38,16 +38,8 @@
 
 #include <math.h>
 
-#include "stat_tool/stat_tools.h"
-#include "stat_tool/regression.h"
-#include "stat_tool/curves.h"
-#include "stat_tool/distribution.h"
-#include "stat_tool/markovian.h"
-#include "stat_tool/vectors.h"
-#include "stat_tool/distance_matrix.h"
 #include "stat_tool/stat_label.h"
 
-#include "sequences.h"
 #include "nonhomogeneous_markov.h"
 #include "sequence_label.h"
 
