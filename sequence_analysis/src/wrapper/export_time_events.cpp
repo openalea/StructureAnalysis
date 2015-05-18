8be3df891476ab2ce3022e36d436b6c7683cafed
Yann Guédon <guedon@cirad.fr>
2015-05-18 15:44:09 +0000

    Change of header inclusion design
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@18305 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- sequence_analysis/src/wrapper/export_time_events.cpp
+++ sequence_analysis/src/wrapper/export_time_events.cpp
@@ -4,7 +4,7 @@
  *
  *        Copyright 2006-2007 INRIA - CIRAD - INRA
  *
- *        File author(s): Yann Guédon <yann.guedon@cirad.fr>
+ *        File author(s): Yann Guedon <yann.guedon@cirad.fr>
  *                        Thomas Cokelaer <Thomas.Cokelaer@inria.fr>
  *
  *        Distributed under the GPL 2.0 License.
@@ -21,13 +21,6 @@
 
 #include "wrapper_util.h"
 
-
-#include "stat_tool/stat_tools.h"
-#include "stat_tool/curves.h"
-#include "stat_tool/distribution.h"
-#include "stat_tool/markovian.h"
-#include "stat_tool/vectors.h"
-#include "stat_tool/distance_matrix.h"
 #include "stat_tool/stat_label.h"
 
 #include "sequence_analysis/sequences.h"
