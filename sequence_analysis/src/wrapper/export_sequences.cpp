f907ef9155be909c206f50317dd3849e1841b5eb
Yann Guédon <guedon@cirad.fr>
2015-05-18 15:43:55 +0000

    Change of header inclusion design
    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@18304 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- sequence_analysis/src/wrapper/export_sequences.cpp
+++ sequence_analysis/src/wrapper/export_sequences.cpp
@@ -22,17 +22,10 @@
 
 #include "wrapper_util.h"
 
-#include "stat_tool/stat_tools.h"
-#include "stat_tool/curves.h"
-#include "stat_tool/distribution.h"
-#include "stat_tool/markovian.h"
-#include "stat_tool/vectors.h"
-#include "stat_tool/distance_matrix.h"
 #include "stat_tool/stat_label.h"
 
 #include "sequence_analysis/sequences.h"
 #include "sequence_analysis/renewal.h"
-// #include "sequence_analysis/tops.h"
 #include "sequence_analysis/sequence_label.h"
 
 #include <boost/python.hpp>
