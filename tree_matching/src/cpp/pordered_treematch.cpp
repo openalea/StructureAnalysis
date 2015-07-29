4f0793cd4c52eba4499c1a47269575d18e08bd57
Yann Guédon <guedon@cirad.fr>
2015-07-29 11:40:15 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@18488 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- tree_matching/src/cpp/pordered_treematch.cpp
+++ tree_matching/src/cpp/pordered_treematch.cpp
@@ -513,7 +513,7 @@ void TreeMatch_PO::standardization()
   int k,i;
   for (k=0;k<nb_fun;k++)
     {
-      if (_vectorDist.get_variable_type(k)==SYMBOLIC)
+      if (_vectorDist.get_var_type(k)==NOMINAL)
         {
           frequency[k] = new int[_vectorDist.get_nb_value(k)];
           for ( i=0;i<_vectorDist.get_nb_value(k);i++)
@@ -540,7 +540,7 @@ void TreeMatch_PO::standardization()
           tree_node=new TreeNode(*_trees[i]->getNode(j));
           for ( k=0;k<nb_fun;k++)
             {
-              if (_vectorDist.get_variable_type(k)==SYMBOLIC)
+              if (_vectorDist.get_var_type(k)==NOMINAL)
                 {
                   frequency[k][int(tree_node->getValue(k))]++;
                   //cout<<frequency[k][int(tree_node->getValue(k))]<<endl;
@@ -567,7 +567,7 @@ void TreeMatch_PO::standardization()
     }
   for (k=0;k<nb_fun;k++)
     {
-      if (_vectorDist.get_variable_type(k)==SYMBOLIC)
+      if (_vectorDist.get_var_type(k)==NOMINAL)
         {
           for (int i=0;i<_vectorDist.get_nb_value(k);i++)
             {
@@ -596,7 +596,7 @@ void TreeMatch_PO::standardization()
           //tree_node=_trees[i]->getNode(j);
           for ( k = 0;k<nb_fun;k++)
             {
-              if (_vectorDist.get_variable_type(k)==SYMBOLIC)
+              if (_vectorDist.get_var_type(k)==NOMINAL)
                 {
                   _dispersion[k]=absd[k];
                 }
