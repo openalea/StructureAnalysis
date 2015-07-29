f79c699134db96707965cba4e572c321c93b48f2
Yann Guédon <guedon@cirad.fr>
2015-07-29 11:36:26 +0000

    git-svn-id: svn+ssh://scm.gforge.inria.fr/svnroot/vplants/vplants/trunk@18475 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- stat_tool/src/wrapper/export_chain.cpp
+++ stat_tool/src/wrapper/export_chain.cpp
@@ -45,9 +45,9 @@ class ChainWrap
 void class_chain()
 {
     class_< Chain >("_Chain", "Chain")
-    .def(init<char, int , bool>())
-    .def(init<char, int, int , bool>())
-    .def(init< optional<char, int, bool> >())
+    .def(init<process_type, int , bool>())
+    .def(init<process_type, int, int , bool>())
+    .def(init< optional<process_type, int, bool> >())
 
     .def(self_ns::str(self)) //__str__
 
